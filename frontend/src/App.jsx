c79b889158821b8b1476912f14bf63b4ab887300
Johan Sebastian Rodas <sebastian.rbcn@gmail.com>
2024-11-03 11:44:44 +0100

    show Metamask selected account and balance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ethers } from "ethers"
+
+const provider = new ethers.BrowserProvider(window.ethereum)
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [account, setAccount] = useState(null)
+  const [balance, setBalance] = useState(null)
+
+  useEffect(() => {
+    window.ethereum.request({method: "eth_requestAccounts"}).then((accounts) => {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        setAccount(accounts[0])
+      })
+      setAccount(accounts[0])
+      console.log(accounts)
+    })
+  }, [])
+
+  useEffect(() => {
+    if(!account) return;
+    provider.getBalance(account).then((balance) => {
+      setBalance(balance.toString())
+    })
+
+  }, [account])
+
 
   return (
-    <div>hello</div>
+    <div>{account} - {balance}</div>
   )
 }
 
