711466f32c8d675d2be59465868600e03289589f
Johan Sebastian Rodas <sebastian.rbcn@gmail.com>
2024-11-03 10:54:51 +0100

    downgrade @openzeppelin/contracts version to use ganache

--- truffle/contracts/Token.sol
+++ truffle/contracts/Token.sol
@@ -6,9 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    constructor(address initialOwner)
-        ERC20("MyToken", "MTK")
-        Ownable(initialOwner)
+    constructor() ERC20("MyToken", "MTK")
     {
         _mint(msg.sender, 10000 * 10 ** decimals());
     }
--- truffle/migrations/2_my_token_deploy.js
+++ truffle/migrations/2_my_token_deploy.js
@@ -4,9 +4,8 @@ module.exports = async function (deployer, network, accounts) {
   // Obtén la cuenta que usaremos como `initialOwner`
   console.log("Hello");
   console.log("Hello: " + accounts[0]);
-  const initialOwner = accounts[0];
 
   // Despliega el contrato usando `deployer.deploy()`
-  await deployer.deploy(MyToken, initialOwner);
+  await deployer.deploy(MyToken);
 };
 
